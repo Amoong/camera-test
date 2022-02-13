e2d96c30485c0b340153a05ca0feaa6ffdb31f90
Amoong <moo1323@naver.com>
2022-02-13 12:24:39 +0900

    update constraint

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
     setMessage("1");
     let constraints = {
       audio: false,
-      video: { height: 2560, width: 1440, facingMode: "environment" },
+      video: { height: 1440, width: 1080, facingMode: "environment" },
     };
     setMessage("2");
 
